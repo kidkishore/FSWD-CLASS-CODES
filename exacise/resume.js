d1ad0fa898ef41b18e5bd9d431c85755b79dc4ee
kidkishore <kidkisore@gmail.com>
2025-07-10 23:37:58 +0530

    Add files via upload

---
+++ exacise/resume.js
@@ -0,0 +1,21 @@
+document.getElementById('ab').onclick = cd()
+{
+            document.getElementById('profile').style.display = 'none';
+            // document.getElementById('hede').innerHTML = 'My Skills:';
+            // document.getElementById('b1').innerHTML = 'Creating and troubleshooting the Electronic circuits.';
+            // document.getElementById('b2').innerHTML = 'Basic Knowledges at the Microsoft Word, Excel and Powerpoint';
+            // document.getElementById('b3').innerHTML = 'Basic Knowledges at programming languages. Like C,C++,Java and python.';
+            // document.getElementById('b4').innerHTML = 'Normal Knowledges at IOT, Networking, Data Analytics, Full Stack Web Development';
+            // document.getElementById('in1').style.display='none';
+            // document.getElementById('in2').style.display='none';
+            // document.getElementById('in3').style.display='none';
+            // document.getElementById('in4').style.display='none';
+            // document.getElementById('ed1').style.display='none';
+            // document.getElementById('ed2').style.display='none';
+            // document.getElementById('ed3').style.display='none';
+            // document.getElementById('ed4').style.display='none';
+            // document.getElementById('dt1').style.display='none';
+            // document.getElementById('dt2').style.display='none';
+            // document.getElementById('dt3').style.display='none';
+            // document.getElementById('dt4').style.display='none';
+        }
