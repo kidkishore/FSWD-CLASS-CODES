7f3c4ee2d1d5c9639332f202e3ebea57e3ebf1e1
kidkishore <kidkisore@gmail.com>
2025-07-10 19:53:33 +0530

    Add files via upload

---
+++ day-9FSWD/portfolio.js
@@ -0,0 +1,14 @@
+function moreu(){
+            document.getElementById("h1").innerHTML = "Basic Developer";
+            document.getElementById("h2").innerHTML = "Intern in Novitech";
+            document.getElementById("h3").innerHTML = "Diploma Graduated(ECE)"
+            document.getElementById("visible").style.display = 'none';
+            document.getElementById("hidden").style.display = 'block';
+        }
+function ome(){
+            document.getElementById("h1").innerHTML = "Hi there";
+            document.getElementById("h2").innerHTML = "This is Kishore j";
+            document.getElementById("h3").innerHTML = "Software developer";
+            document.getElementById("visible").style.display = 'block';
+            document.getElementById("hidden").style.display = 'none';
+        }
